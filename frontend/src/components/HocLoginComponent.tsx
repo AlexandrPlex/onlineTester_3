4ec7242493415f369bdb4f8836395fe61be70960
Aplex <fos21@mail.ru>
2018-06-29 18:52:19 +0300

    4

--- frontend/src/components/HocLoginComponent.tsx
+++ frontend/src/components/HocLoginComponent.tsx
@@ -3,11 +3,11 @@ import * as React from 'react';
 import { asyncComponent } from 'react-async-component';
 import {getRoleRequestFetch} from '../api/hocAuthApi';
 
-const HocComponet = (Component: any) => {
+const HocLoginComponet = (Component: any) => {
   return asyncComponent({
     resolve: async () => {
       const data = await getRoleRequestFetch();
-      return class extends React.Component<{}, {auth: boolean, role_type: string}> {
+      return class extends React.Component<{}, {auth: boolean}> {
         public static contextTypes = {
           router: PropTypes.object,
         };
@@ -15,25 +15,17 @@ const HocComponet = (Component: any) => {
           super(props, context);
           this.state = {
             auth: data.auth,
-            role_type: data.role_type,
           };
         }
         public componentWillMount() {
-          this.setState({auth: data.auth, role_type: data.role_type});
+          this.setState({auth: data.auth});
         }
-        public onRedirect = (url: string) => {
-          return document.location.replace(url);
-        }
-        public onRoute = () => {
-          if (this.state.role_type === document.location.pathname) {
-            return <Component/>;
-          }
-          return this.context.router.history.push('/404');
-        }
+        public onAuthTrue = () => {};
+        public onAuthFalse = () => {};
         public render() {
           console.log(this.state.auth);
           return <div> {
-            !this.state.auth ? this.onRedirect('/') : this.onRoute()
+            this.state.auth ? this.onAuthTrue : this.onAuthFalse
           } </div>;
         }
       };
@@ -41,4 +33,4 @@ const HocComponet = (Component: any) => {
     LoadingComponent: () => <h1>Loading product</h1>,
   });
 };
-export default HocComponet;
+export default HocLoginComponet;
